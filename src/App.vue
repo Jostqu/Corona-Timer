6720af4032b2db21e342f80bcc53660a86dbe62d
Jost Quante <jq379897@fh-muenster.de>
2020-12-16 00:06:38 +0100

    fix timer problem when deleted while running

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="container">
+  <div class="container-fluid no-padding .container">
     <button class="btn btn-light" @click="addnewTimer">Add</button>
-
-    <div class="row" v-bind:key="i" v-for="(timer,i) in groupedTimers">
-      <div v-for="(timer,index) in groupedTimers[i]" v-bind:key="index" class="col-lg-4">
-        <div class="card-body">
-        <span class="float-right" style="cursor:pointer" @click="removeTimer()">X</span>
-        <h4 class="card-title"> Test {{i*3+index+1}}</h4>
-        <div v-if="timers[i*3+index].time >= 120000 && timers[i*3+index].time < 121000 && timers[i*3+index].section === 0" class="alert alert-danger blink_me" role="alert">
+    <button class="btn btn-light mx-1" @click="removeTimer()">Remove</button>
+    <div class="row no-padding" v-bind:key="i" v-for="(row,i) in groupedTimers">
+      <div v-for="(timer_col,index) in groupedTimers[i]" v-bind:key="index" class="col-sm no-padding">
+        <div class="card-body no-padding">
+        <h6 class="card-title no-padding"> Test {{i*per_collum+index+1}}</h6>
+        <div v-if="timers[i*per_collum+index].time >= 120000" class="alert alert-danger no-padding .alert" role="alert">
           Testlösung auftragen!</div>
-        <h6 v-if="timers[i*3+index].time > 0 && timers[i*3+index].time < 120000">Tupfer in Testlösung</h6>
-        <h6 v-if="timers[i*3+index].time > 120000 && timers[i*3+index].time < 1020000">Test läuft</h6>
-        <div v-if="timers[i*3+index]._2time >= 900000" class="alert alert-success blink_me" role="alert">
+        <h6 v-if="timer_col.time > 0 && timer_col.time < 120000">
+          Tupfer in Testlösung</h6>
+        <h6 v-if="timer_col._2time > 0 && timer_col._2time < 900000">
+          Test läuft</h6>
+        <div v-if="timer_col._2time >= 900000" class="alert alert-success .alert" role="alert">
         Ergebnis ablesen!</div>
         <div class="timer-form">
           <div class="progress">
-            <div class="progress-bar pg-beginning" role="progressbar"  v-bind:style="{ width: timers[i*3+index].time/1000*0.0980392156862742 + '%'}" v-bind:aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
-            <div class="progress-bar bg-success" role="progressbar"  v-bind:style="{width: ((timers[i*3+index]._2time/1000*0.0980392156862742)) + '%'}" v-bind:aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar pg-beginning" role="progressbar"  v-bind:style="{ width: timers[i*per_collum+index].time/1000*0.0980392156862742 + '%'}" v-bind:aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-warning" role="progressbar"  v-bind:style="{width: ((timers[i*per_collum+index]._2time/1000*0.0980392156862742)) + '%'}" v-bind:aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
-          <div class="display:inline" v-if="timer.section === 0">{{formattedElapsedTime(timer.time)}}</div>
-          <div class="display:inline" v-if="timer.section === 1">{{formattedElapsedTime(timer._2time)}}</div>
-          <button class="btn btn-success" @click="start(i*3+index)" v-if="timers[i*3+index].time === 0">start</button>
-          <button class="btn btn-success" @click="start(i*3+index)" v-if="timers[i*3+index].time >= 120000 && timers[i*3+index]._2time <= 0 ">Weiter</button>
-          <button class="btn btn-danger"  @click="reset(i*3+index)">Reset</button>
+          <div class="display:inline" v-if="timer_col.section === 0">{{formattedElapsedTime(timer_col.time)}}</div>
+          <div class="display:inline" v-if="timer_col.section === 1">{{formattedElapsedTime(timer_col._2time)}}</div>
+          <button class="btn  btn-success "  @click="start(i*per_collum+index)" v-if="timers[i*per_collum+index].time === 0">Start</button>
+          <button class="btn  btn-success "  @click="start(i*per_collum+index)" v-if="timers[i*per_collum+index].time >= 120000 && timers[i*per_collum+index]._2time <= 0 ">Weiter</button>
+          <button class="btn btn-danger "  @click="reset(i*per_collum+index)">Reset</button>
         </div>
       </div>
     </div>
@@ -31,6 +32,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'app',
   comments:{
@@ -38,10 +40,8 @@ export default {
   },
   data() {
     return {
-      timer:[{
-        timer: undefined
-      }
-      ],
+      per_collum: 5,
+      audio: new Audio(),
       timers:[
         {
           Name: '',
@@ -50,7 +50,8 @@ export default {
           run: 0,
           section: 0,
           _2time: 0,
-          time: 0
+          time: 0,
+          timer: undefined
         }
       ]
     }
@@ -58,18 +59,15 @@ export default {
   ,
   computed:{
     groupedTimers() {
-      return _.chunk(this.timers,3)
+      return _.chunk(this.timers,5)
     }
   },
   methods: {
     removeTimer(){
-      this.timer.pop()
+      clearInterval(this.timers[this.timers.length-1].timer);
       this.timers.pop()
     },
     addnewTimer() {
-      this.timer.push({
-        timer: undefined
-      })
       this.timers.push({
         Name: '',
         start: 0,
@@ -77,10 +75,13 @@ export default {
         run: 0,
         section: 0,
         _2time: 0,
-        time: 0
+        time: 0,
+        timer: undefined
       })
     },
     start(index) {
+      this.audio.play();
+      this.audio.src = require('./test.mp3');
       if(this.timers[index].time <=120000){
         this.timers[index].start = Date.now()
       }
@@ -91,30 +92,30 @@ export default {
       if(this.timers[index].time >= 120000){
         this.timers[index].section = 1;
       }
-      this.timer[index] = setInterval(() => {
+      this.timers[index].timer = setInterval(() => {
+        if(this.timers[index].run === 0){
+          clearInterval(this.timers[index].timer)
+        }
         if(this.timers[index].section === 0 ){
           this.timers[index].time = Date.now()-this.timers[index].start;
         }else{
           this.timers[index]._2time = Date.now()-this.timers[index]._2Start;
         }
-        if(this.timers[index].time >= 120000 && this.timers[index].time < 121000 && this.timers[index].section !== 1 ){
-          clearInterval(this.timer[index].timer);
+        if(this.timers[index].time >= 120000 && this.timers[index].section !== 1 ){
+          clearInterval(this.timers[index].timer);
           this.timers[index].run =0;
-          const audio = new Audio(require('./234524_foolboymedia_notification-up-1 (online-audio-converter.com).mp3'));
-          audio.play();
+          this.audio.play();
         }
         if(this.timers[index]._2time >= 900000){
-          clearInterval(this.timer[index].timer);
+          clearInterval(this.timers[index].timer);
           this.timers[index].run =0;
-          const audio = new Audio(require('./234524_foolboymedia_notification-up-1 (online-audio-converter.com).mp3'));
-          audio.play();
+          this.audio.play();
         }
 
       }, 1000);
     },
     reset(index) {
-
-      clearInterval(this.timer[index].timer);
+      clearInterval(this.timers[index].timer);
       this.timers[index].time = 0;
       this.timers[index].section = 0;
       this.timers[index].run = 0;
@@ -134,5 +135,7 @@ export default {
 </script>
 
 <style>
-
+.alert {
+  font-size: 70%;
+}
 </style>
